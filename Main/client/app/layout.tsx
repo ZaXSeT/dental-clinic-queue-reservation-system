bfd1fe4a8e43363981c73b0075255581902d1566
Zacky Setiawan <zackyxie123@gmail.com>
2026-04-17 00:27:09 +0700

    revert: layout font to jakarta.className

--- Main/client/app/layout.tsx
+++ Main/client/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className="scroll-smooth">
-            <body className={`${jakarta.variable} font-sans`}>
+            <body className={jakarta.className}>
                 <Suspense fallback={null}>
                     {children}
                 </Suspense>
